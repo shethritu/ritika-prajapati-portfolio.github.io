b80760bc8b8111af816152f08e6c1fb6c0865344
shethritu <96720702+shethritu@users.noreply.github.com>
2024-09-07 23:38:34 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// Initialize matchHeight on all elements with the class 'box'
+$(document).ready(function() {
+    $('.service-block-one .card__content p').matchHeight();
+    $('.service-block-one .icon-box').matchHeight();
+});
