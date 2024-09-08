851bb1ef69948c0e7b0163812f2dc3890f5f5f94
shethritu <96720702+shethritu@users.noreply.github.com>
2024-09-08 12:32:51 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,19 @@
 // Initialize matchHeight on all elements with the class 'box'
 $(document).ready(function() {
-    $('.service-block-one .card__content p').matchHeight();
-    $('.service-block-one .icon-box').matchHeight();
+$('.service-block-one .card__content p').matchHeight();
+$('.service-block-one .icon-box').matchHeight();
+});
+
+
+$(window).scroll(function() {
+if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(200);    // Fade in the arrow
+} else {
+    $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+}
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+$('body,html').animate({
+    scrollTop : 0                       // Scroll to top of body
+}, 500);
 });
