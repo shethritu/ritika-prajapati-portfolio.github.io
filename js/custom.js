a6bdc60721cd3536e4fd864e0baa491f76449af2
shethritu <96720702+shethritu@users.noreply.github.com>
2024-09-08 12:15:29 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,23 +5,3 @@ $(document).ready(function() {
 });
 
 
-		// Define a media query
-const mediaQuery = window.matchMedia('(max-width: 767px)');
-
-// Function to handle changes in media query status
-function handleScreenChange(e) {
-  if (e.matches) {
-    // If the screen is less than or equal to 768px
-    document.querySelectorAll('#myLinks .nav-link').forEach(item => {
-		item.addEventListener('click', () => {
-		document.getElementById('myLinks').style.display = 'none'; // Hides the menu
-		});
-	});
-  }
-}
-
-// Initial check
-handleScreenChange(mediaQuery);
-
-// Add a listener for when the media query status changes
-mediaQuery.addListener(handleScreenChange);
