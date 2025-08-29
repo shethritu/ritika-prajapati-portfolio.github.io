580622536962548ec4e9a34de5b803ff0c8cb00f
shethritu <96720702+shethritu@users.noreply.github.com>
2025-08-29 13:51:00 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,57 @@
 // Initialize matchHeight on all elements with the class 'box'
 $(document).ready(function() {
-$('.service-block-one .card__content p').matchHeight();
-$('.service-block-one .icon-box').matchHeight();
+    $('.service-block-one .card__content p').matchHeight();
+    $('.service-block-one .icon-box').matchHeight();
 });
 
 
 $(window).scroll(function() {
-if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-    $('#return-to-top').fadeIn(200);    // Fade in the arrow
-} else {
-    $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-}
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
 });
 $('#return-to-top').click(function() {      // When arrow is clicked
-$('body,html').animate({
-    scrollTop : 0                       // Scroll to top of body
-}, 500);
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+// header
+$(document).ready(function () {
+    $("#header .main-nav .click-menu").click(function () {
+    $("#header .main-nav nav").addClass("show");
+    $("#header .main-nav .cancel-menu").addClass("show");
+    $("body").css({ position: "fixed", width: "100%" });
+    });
+
+    $("#header .main-nav .cancel-menu").click(function () {
+    $("#header .main-nav nav").removeClass("show");
+    $("#header .main-nav .cancel-menu").removeClass("show");
+    $("body").css({ position: "unset", width: "auto" });
+    });
+
+    $("#header nav ul li a").click(function () {
+    $("#header .main-nav nav").removeClass("show");
+    $("#header .main-nav .cancel-menu").removeClass("show");
+    $("body").css({ position: "unset", width: "auto" });
+    });
+
+     // aos
+    AOS.init({
+        duration: 1000,
+    });
+
+    // sticky header
+    if($(window).width () >= 991) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 200) {
+                $('#header').addClass('show');
+            }
+            else {
+                $('#header').removeClass('show');
+            }
+        });
+    }
 });
